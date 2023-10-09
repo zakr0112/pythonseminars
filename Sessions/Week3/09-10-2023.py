32077c26e1ca45347ea94ae7dcad78dfe7c3bc7a
zakr0112 <145971994+zakr0112@users.noreply.github.com>
2023-10-09 09:35:37 +0000

    Updated packages to include numpy as np

--- Sessions/Week3/09-10-2023.py
+++ Sessions/Week3/09-10-2023.py
@@ -1,5 +1,6 @@
 #imports
 import time
+import numpy as np
 
 #Variable recap
 
@@ -12,4 +13,53 @@ time.sleep(1)
 knights = []
 knights.append('Zak')
 
-#Temperature Program
+# task1
+print("Task 1")
+print(10 < 100)
+print(100 != 100)
+print(50 >= 50)
+print()
+"""
+True
+False
+True
+
+Works as intended
+"""
+
+# task2
+print("Task 2")
+print("a" < "b")
+print("b" < "a")
+print("John" < "Terry")
+print()
+
+#Marks Calculation
+
+"""
+Five emams - five different marks
+- Collect all marks in variables
+"""
+
+
+exam1 = int(input("Enter mark 1: "))
+
+exam2 = int(input("Enter mark 2: "))
+
+exam3 = int(input("Enter mark 3: "))
+
+exam4 = int(input("Enter mark 4: "))
+
+exam5 = int(input("Enter mark 5: "))
+
+exams = [exam1, exam2, exam3, exam4, exam5]
+
+print(exams)
+
+highest = max(exams)
+print("Highest:", highest)
+
+lowest = min(exams)
+print("Lowest: ", lowest)
+
+print("Mean :", np.mean(exams))
