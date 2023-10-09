4590a5462f314001280f5f9159947afe90010c9d
zakr0112 <145971994+zakr0112@users.noreply.github.com>
2023-10-09 08:43:43 +0000

    Changed file locations

---
+++ Sessions/main.py
@@ -0,0 +1,18 @@
+#imports
+import time
+
+#Variable recap
+
+order = 'eggs' + ' ' + 'spam'
+order = 'eggs' + ' ' + 'spam' * 3
+
+order = order[3:-1]
+time.sleep(1)
+print(order)
+
+knights = []
+knights.append('Zak')
+print(knights)
+
+#Builtin
+#Max - Min
--- main.py
+++
@@ -1,9 +0,0 @@
-
-name = "zak"
-bold_knights = 3
-requiredpassword = True
-
-print(name)
-print(bold_knights)
-print(requiredpassword)
-
