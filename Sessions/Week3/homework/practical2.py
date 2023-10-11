8da90a9ea4b417e0fb6943a4d83d5fc8bd9b3333
zakr0112 <zakofzaaz@gmail.com>
2023-10-11 15:54:16 +0000

    Completed

--- Sessions/Week3/homework/practical2.py
+++ Sessions/Week3/homework/practical2.py
@@ -1,1 +1,20 @@
 # Practical 2
+def passwordentry():
+  password1 = input("Enter password: ")
+  password2 = input("Re-enter password: ")
+  if password1 == password2:
+    passwordsuccess()
+  else:
+    print()
+    print("Passwords don't match")
+    print()
+    passwordentry()
+
+def passwordsuccess():
+  print()
+  print("Passwords Match!")
+  print("Access Granted")
+  print()
+
+
+passwordentry()
