9e10d187a137afaf64e4a9406b706d1c364e9182
zakr0112 <zakofzaaz@gmail.com>
2023-10-11 16:18:43 +0000

    Completed

--- Sessions/Week3/homework/practical2.py
+++ Sessions/Week3/homework/practical2.py
@@ -1,9 +1,23 @@
 # Practical 2 - Password Entry Program
+BAD_PASSWORDS = ['password', 'letmein', 'sesame', 'hello', 'justinbieber']
+
+
 def passwordentry():
   password1 = input("Enter password: ")
   password2 = input("Re-enter password: ")
-  if password1 == password2:
-    passwordsuccess()
+  if password1 in BAD_PASSWORDS or password2 in BAD_PASSWORDS:
+    print()
+    print("Oops! This is a common password")
+    print()
+    passwordentry()
+  elif password1 == password2:
+    if len(password1) >= 8 and len(password2) <= 12:
+      passwordsuccess()
+    else:
+      print()
+      print("Passwords should be between 8 and 12 characters long")
+      print()
+      passwordentry()
   else:
     print()
     print("Passwords don't match")
@@ -14,8 +28,9 @@ def passwordentry():
 def passwordsuccess():
   print()
   print("Passwords Match!")
-  print("Access Granted")
   print()
 
 
 passwordentry()
+  
+
