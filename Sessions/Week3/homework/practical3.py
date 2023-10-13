62dc968ee86d199bdcac7da426cfdcfb1c80857c
zakr0112 <zakofzaaz@gmail.com>
2023-10-13 00:57:08 +0000

    Completed

--- Sessions/Week3/homework/practical3.py
+++ Sessions/Week3/homework/practical3.py
@@ -1,1 +1,15 @@
 # Practical 3 - Times Table Program
+
+for i in range(0, 13):
+  print(f"7 x {i} = {7 * i}")
+
+userentry = int(input("Enter a number: "))
+while userentry < 0 or userentry > 12:
+  userentry = int(input("Enter a number: "))
+
+calculation = userentry * 7
+print(f"{userentry} x 7 = {calculation}")
+while userentry < 12:
+  userentry += 1
+  calculation = userentry * 7
+  print(f"{userentry} x 7 = {calculation}")
