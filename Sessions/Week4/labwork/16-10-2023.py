80011ee2959a3f80741352b7dd0ebd4e53dbb917
zakr0112 <zakofzaaz@gmail.com>
2023-10-16 10:57:43 +0000

    Updated

---
+++ Sessions/Week4/labwork/16-10-2023.py
@@ -0,0 +1,24 @@
+#from math import *##
+#import math
+#from math import sqrt
+from math import pi
+
+
+"""
+result = math.sin(6.2)
+root = sqrt(49)
+val = cos(radians(45))
+
+"""
+
+print("The value of pi is", pi)
+
+
+def displayTwice(msg):
+  print(msg)
+  print(msg)
+
+displayTwice()
+
+def findMax(a,b):
+
