650e64040346840d72eb5dd66681ad35bf0ebabb
zakr0112 <zakofzaaz@gmail.com>
2023-10-20 18:50:32 +0000

    Added

---
+++ Sessions/Week4/homework/practical2.py
@@ -0,0 +1,5 @@
+# Practical 2 - Function String Test
+"""
+Write a function that has a single string as its parameter, and returns the number of
+uppercase letters, and the number of lowercase letters in the string
+"""
---
+++ Sessions/Week4/homework/practical3.py
@@ -0,0 +1,14 @@
+# Practical 3 - Greetings Program Modified
+"""
+Modify your "greetings" program so that the first letter of the name entered is
+always in uppercase with the rest in lowercase. This should happen even if the user
+entered their name differently. So if the user entered arthur, ARTHUR, or even
+arTHur the name should be displayed as Arthur.
+"""
+
+print()
+name = input("Enter name: ")
+if name == "":
+  print("Hello stranger")
+else:
+  print("Hello", name)
