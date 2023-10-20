016219ae5c61c1101879bb575f287f42d5073a8c
zakr0112 <zakofzaaz@gmail.com>
2023-10-20 18:48:07 +0000

    Completed

--- Sessions/Week4/homework/practical1.py
+++ Sessions/Week4/homework/practical1.py
@@ -1,1 +1,17 @@
 # Practical 1 - Function Test
+"""
+Functions are often used to validate input. Write a function that accepts a single
+integer as a parameter and returns True if the integer is in the range 0 to 100
+(inclusive), or False otherwise
+"""
+
+def ValidInput():
+  print()
+  validNum = int(input("Enter integar between 0 and 100: "))
+  if validNum < 0 or validNum > 100:
+        print("False")
+  else:
+        print("True")
+    
+
+ValidInput()
