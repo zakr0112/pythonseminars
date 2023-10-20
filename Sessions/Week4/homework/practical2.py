05e558a62710d36672edeff0ac43e15ed9421059
zakr0112 <zakofzaaz@gmail.com>
2023-10-20 19:04:51 +0000

    Completed

--- Sessions/Week4/homework/practical2.py
+++ Sessions/Week4/homework/practical2.py
@@ -3,3 +3,21 @@
 Write a function that has a single string as its parameter, and returns the number of
 uppercase letters, and the number of lowercase letters in the string
 """
+
+def start():
+  print()
+  userInput = input("Enter some text: ")
+  uppercase_count = 0
+  lowercase_count = 0
+  for letter in userInput:
+    if letter.isupper():
+      uppercase_count += 1
+    elif letter.islower():
+      lowercase_count += 1
+
+  print("Uppercase:", uppercase_count)
+  print("Lowercase: ", lowercase_count)
+
+
+
+start()
