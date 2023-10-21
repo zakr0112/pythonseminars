8987c6558029ef1fb4caf3e090eed6e1bc66457e
zakr0112 <zakofzaaz@gmail.com>
2023-10-21 13:32:04 +0000

    Staged for completion

--- Sessions/Week4/homework/practical4.py
+++ Sessions/Week4/homework/practical4.py
@@ -1,1 +1,9 @@
 # Practical 4 - String Character Paramter
+"""
+When processing data it is often useful to remove the last character from some
+input (it is often a newline). Write and test a function that takes a string parameter
+and returns it with the last character removed. (If the string contains one or fewer
+characters, return it unchanged.)
+"""
+
+print()
