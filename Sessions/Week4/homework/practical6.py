1586ce5381e7ca13aa6bd583ad9c683a5423da55
zakr0112 <zakofzaaz@gmail.com>
2023-10-21 17:09:01 +0000

    Added

--- Sessions/Week4/homework/practical6.py
+++ Sessions/Week4/homework/practical6.py
@@ -1,1 +1,8 @@
 # Practical 6 - Centigrade temp into fahrenheit, and the reverse
+"""
+Write a program that takes a centigrade temperature and displays the equivalent in
+fahrenheit. The input should be a number followed by a letter C. The output should
+be in the same format
+"""
+
+print()
