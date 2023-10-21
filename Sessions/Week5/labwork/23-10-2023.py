3a444737c9cdcdd1c8626acdd28c7f9c1d30cc6e
zakr0112 <zakofzaaz@gmail.com>
2023-10-21 19:11:36 +0000

    Modified

--- Sessions/Week5/labwork/23-10-2023.py
+++ Sessions/Week5/labwork/23-10-2023.py
@@ -1,1 +1,12 @@
 #Labwork - 23/10/2023
+
+number = input("Enter a number: ")
+
+number = int(number)
+
+print("The numbered entered was", number)
+
+if (number % 2) == 0:
+  print("That is an even number")
+else:
+  print("That is an odd number")
