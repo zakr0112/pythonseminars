f5440dfd94ed830739e5457a8ed02fa99e0bec60
zakr0112 <zakofzaaz@gmail.com>
2023-10-21 16:15:30 +0000

    Added

--- Sessions/Week4/homework/practical5.py
+++ Sessions/Week4/homework/practical5.py
@@ -1,1 +1,8 @@
 # Practical 5 - Convert Temparatures
+"""
+Write and test a function that converts a temperature measured in degrees
+centigrade into the equivalent in fahrenheit, and another that does the reverse
+conversion. Test both functions. (Google will find you the formulae).
+"""
+
+print()
