9e39f8cf4d3c075dca3d00065bbfb8520526b55a
zakr0112 <zakofzaaz@gmail.com>
2023-10-23 09:16:01 +0000

    Updated

--- Sessions/Week5/homework/practical3.py
+++ Sessions/Week5/homework/practical3.py
@@ -3,3 +3,13 @@
 Write a program that takes a bunch of command-line arguments, and then prints
 out the shortest. If there is more than one of the shortest length, any will do
 """
+
+import sys
+
+number = len(sys.argv)
+parsed = number - 1
+print("Arguments passed", parsed)
+print("")
+
+
+# Will complete later (stuck rn)
