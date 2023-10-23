28949afde4135dfbde6b8bb7fc23f3fefbe4fb1a
zakr0112 <zakofzaaz@gmail.com>
2023-10-23 09:07:49 +0000

    Completed

--- Sessions/Week5/homework/practical2.py
+++ Sessions/Week5/homework/practical2.py
@@ -4,3 +4,9 @@ Write a program that, when run from the command line, reports how many
 arguments were provided. (Remember that the program name itself is not an
 argument).
 """
+
+import sys
+
+number = len(sys.argv)
+parsed = number - 1
+print("Arguments passed", parsed)
