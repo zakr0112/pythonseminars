deae27316665a7a9ce25090cab0841740e2d3ab5
zakr0112 <zakofzaaz@gmail.com>
2023-10-23 09:31:59 +0000

    Updated

--- Sessions/Week5/homework/practical4.py
+++ Sessions/Week5/homework/practical4.py
@@ -3,3 +3,9 @@
 Write a program that takes a URL as a command-line argument and reports
 whether or not there is a working website at that address.
 """
+
+import sys, getopt
+import argparse
+
+
+website = input("Enter website: ")
