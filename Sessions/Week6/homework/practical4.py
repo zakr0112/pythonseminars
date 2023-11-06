e494c3e290fa24284d8653e4d3bea373be3e3b39
zakr0112 <zakofzaaz@gmail.com>
2023-11-06 08:46:20 +0000

    Updated

--- Sessions/Week6/homework/practical4.py
+++ Sessions/Week6/homework/practical4.py
@@ -5,3 +5,12 @@ Computers are commonly used in encryption. A very simple form of encryption
 and reverse the resulting string. Write, and test, a function that takes a string
 containing a message and "encrypts" it in this way.
 """
+
+print()
+
+message = input("Enter your string: ")
+
+def remove_space(message):
+  return message.replace(" ", "")
+
+print("Part 1:", remove_space(message))
