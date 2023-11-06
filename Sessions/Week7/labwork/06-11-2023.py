03fadb40f9b5496c97a308eb594fcedf7804db4e
zakr0112 <zakofzaaz@gmail.com>
2023-11-06 09:19:51 +0000

    Updated

---
+++ Sessions/Week7/labwork/06-11-2023.py
@@ -0,0 +1,11 @@
+vowels = {"a", "e", "i", "o", "u"}
+names = {"John", "Eric", "Terry", "Michael", "Graham", "Terry"}
+print(names)
+
+
+
+
+
+hex_letters = {"a", "b", "c", "d", "e", "f"}
+hex_letters = set("abcdef")
+print(hex_letters)
