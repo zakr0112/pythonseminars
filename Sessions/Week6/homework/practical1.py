fb16abb01fab3c9c01593c25413c443e569a7cc9
zakr0112 <zakofzaaz@gmail.com>
2023-11-06 08:30:44 +0000

    Completed

--- Sessions/Week6/homework/practical1.py
+++ Sessions/Week6/homework/practical1.py
@@ -3,3 +3,10 @@
 Write a function that accepts a positive integer as a parameter and then returns a
 representation of that number in binary (base 2).
 """
+
+print()
+IntInput = int(input("Enter Integar: "))
+print()
+bin_IntInput = bin(IntInput)
+print("Binary:", bin_IntInput)
+print("Denary", IntInput)
