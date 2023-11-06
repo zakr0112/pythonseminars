79ff4433f00ee570e61e112b998ebabf66d716fe
zakr0112 <zakofzaaz@gmail.com>
2023-11-06 08:32:19 +0000

    Updated

--- Sessions/Week6/homework/practical2.py
+++ Sessions/Week6/homework/practical2.py
@@ -1,6 +1,8 @@
 #Practical 2 - Factors Parameter Function
 """
 Write and test a function that takes an integer as its parameter and returns the
-factors of that integer. (A factor is an integer which can be multiplied by another to
-yield the original).
+factors of that integer. (A factor is an integer which can be multiplied by another to yield the original).
 """
+
+print()
+IntInput = int(input("Enter Integar: "))
