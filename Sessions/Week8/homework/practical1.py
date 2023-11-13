bfc96a62c748576dba1dfad8b29520b7b9367b80
zakr0112 <zakofzaaz@gmail.com>
2023-11-13 10:21:49 +0000

    Updated

--- Sessions/Week8/homework/practical1.py
+++ Sessions/Week8/homework/practical1.py
@@ -1,9 +1,13 @@
 #Practical 1 - Unix nl (CLA Implementation)
 """
 The Unix nl command prints the lines of a text file with a line number at the start
-of each line. (It can be useful when printing out programs for dry runs or white-box
-testing). Write an implementation of this command. It should take the name of the
+of each line. (It can be useful when printing out programs for 
+dry runs or white-box testing).
+
+Write an implementation of this command. It should take the name of the
 files as a command-line argument
 """
+import os
+import sys
+import argv
 
-print()
