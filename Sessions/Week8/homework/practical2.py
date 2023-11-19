55e3012e444752130599adf7b3acb87454eb5b15
zakr0112 <zakofzaaz@gmail.com>
2023-11-19 22:41:13 +0000

    Updated

--- Sessions/Week8/homework/practical2.py
+++ Sessions/Week8/homework/practical2.py
@@ -5,5 +5,24 @@ Write a simple implementation of this that takes two file names as command-line
 arguments and reports whether or not the two files are the same. (Define "same" as
 having the same contents.)
 """
-
 print()
+
+import sys
+
+
+
+filename = sys.argv[1]
+filename2 = sys.argv[2]
+
+with open(sys.argv[1]) as filename:
+  for line in filename:
+         print(line)
+
+with open(sys.argv[2]) as filename2:
+  for line in filename2:
+         print(line)
+
+if filename == filename2:
+  print("The file contents are the same")
+else:
+  print("The file contents are different")
