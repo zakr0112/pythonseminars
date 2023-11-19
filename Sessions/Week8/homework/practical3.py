0e766741a9c5a142ee553dd97835d22e221116b2
zakr0112 <zakofzaaz@gmail.com>
2023-11-19 22:56:07 +0000

    Updated

--- Sessions/Week8/homework/practical3.py
+++ Sessions/Week8/homework/practical3.py
@@ -5,6 +5,7 @@ contain a certain pattern. Write an implementation of this. It will take two
 command-line arguments: the first is the string to look for, and the second is the
 file name. The output should be the lines in the file that contain the string.
 """
+import sys
 
 print()
 
@@ -12,6 +13,11 @@ grepFileName = sys.argv[1]
 stringInFile = sys.argv[2]
 
 with open(sys.argv[1]) as grepFileName:
+  lines = grepFileName.readlines()
+  matchingString = [line.strip() for line in lines if stringInFile in line]
+  if matchingString:
+    for line in matchingString:
+      print(line)
   
 
 
