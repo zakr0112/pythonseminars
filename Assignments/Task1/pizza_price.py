d2e511da0fc1ac6c0716a003184ab468724a3a7b
zakr0112 <zakofzaaz@gmail.com>
2023-11-27 10:38:29 +0000

    Updated

--- Assignments/Task1/pizza_price.py
+++ Assignments/Task1/pizza_price.py
@@ -3,10 +3,10 @@ import time
 time.gmtime(0)
 
 #Global
-pizzaNumber = 0
+
+
 basePrice = 12
-deliveryCost = 2.50
-freeDelivery = 0
+
 
 
 def BPP():
@@ -19,6 +19,8 @@ def BPP():
 
 def startOrder():
   try:
+    global pizzaNumber
+    
     pizzaNumber = int(input("Pizzas: "))
     if pizzaNumber == 0 or pizzaNumber < 1:
       print("Oops! Please enter a higher number")
@@ -33,23 +35,75 @@ def startOrder():
   deliveryStart()
 
 def deliveryStart():
+  global deliveryReq
+  global deliveryCost
+  deliveryCost = 2.50
   deliveryReq = input("Is delivery required? (y/n): ")
   if deliveryReq == "y":
     if pizzaNumber >= 5:
-      print("Delivery Cost:", freeDelivery)
+      deliveryCost = deliveryCost - 2.50
+      print("Delivery Cost:", deliveryCost)
+      tuesdayDiscount()
     elif pizzaNumber < 5:
       print("Delivery Cost:", deliveryCost)
+      tuesdayDiscount()
   elif deliveryReq == "n":
     print("Pickup Cost: 0")
+    tuesdayDiscount()
   else:
     deliveryStart()
 
+def tuesdayDiscount():
+  global tuesdayOrNot
+  tuesdayOrNot = input("Is it tuesday? (y/n): ")
+  if tuesdayOrNot == "y":
+    print("50% Discount Applied")
+  elif tuesdayOrNot == "n":
+    print("No Discount Available")
+  else:
+    tuesdayDiscount()
+  bppAPP()
+
+def bppAPP():
+  global bppOrNot
+  bppOrNot = input("Was the BPP app used? (y/n): ")
+  if bppOrNot == "y":
+    print("25% Discount Applied")
+  elif bppOrNot == "n":
+    print("No Discount Available")
+  else:
+    bppAPP()
+  orderCalculation()
+
+def orderCalculation():
+  subtotal = basePrice * pizzaNumber + deliveryCost
+  
   print()
   print("===================")
+  print("Pizza's:", pizzaNumber)
+  print("Delivery Cost:", deliveryCost)
+    
+  if tuesdayOrNot == "y":
+    print("Tuesday Discount: 50% (y)")
+    discount1 = subtotal / 2
+  elif tuesdayOrNot == "n":
+    print("Tuesday Discount: 0% (n)")
+    discount1 = 0
 
+  if bppOrNot == "y":
+    print("BPP App Discount: 25% (y)")
+    discount2 = subtotal / 4
+  elif bppOrNot == "n":
+    print("BPP App Discount: 0% (y)")
+    discount2 = 0
+
+  fulltotal = subtotal - discount1 - discount2
   print()
-
-
+  print("Subtotal:", subtotal)
+  print("Discount 1:", discount1)
+  print("Discount 2:", discount2)
+  print("Total:", fulltotal)
+  print()
 
 #start
 BPP()
