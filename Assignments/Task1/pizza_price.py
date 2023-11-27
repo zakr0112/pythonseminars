b685e88faa5dc1a16a69ff44bb2a5e97a108647a
zakr0112 <zakofzaaz@gmail.com>
2023-11-27 09:45:37 +0000

    Updated

--- Assignments/Task1/pizza_price.py
+++ Assignments/Task1/pizza_price.py
@@ -2,12 +2,39 @@
 import time
 time.gmtime(0)
 
+#Global
+pizzaNumber = 0
+toppings = ""
+base = 12
+
+
 def BPP():
   time.sleep(1)
   print("Beckett Pizza Plaza")
   print("===================")
+  time.sleep(1)
+  startOrder()
 
 
+def startOrder():
+  try:
+    pizzaNumber = int(input("Pizzas: "))
+    if pizzaNumber > 5:
+      print("Free delivery!")
+    elif pizzaNumber < 5:
+      print("Delivery fee will be charged")
+  except ValueError:
+    print("This is not a value!")
+    time.sleep(1)
+    startOrder()
+  
+  
+  toppings = input("Toppings: ")
+  tuesdayOrNot = input("Tuesday?: ")
+  print()
+  print("===================")
+
+  print()
 
 
 
