cf5cf4876a718c2df08fcea21e2f841f8314b12b
zakr0112 <zakofzaaz@gmail.com>
2023-12-21 19:30:11 +0000

    Updated

---
+++ Assignments/Task3/passwordTask3.py
@@ -0,0 +1,15 @@
+def menu():
+  print("1: Add User")
+  print("2: Delete User")
+  print("3: Change Password")
+  print("4: Login")
+  menuOption = 
+
+
+def addUser():
+  with open("passwd.txt") as file:
+
+
+
+#run
+menu()
