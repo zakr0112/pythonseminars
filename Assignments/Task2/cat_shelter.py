08a7a100ce971fe4882a013051573bf422515a45
zakr0112 <zakofzaaz@gmail.com>
2023-12-21 13:27:23 +0000

    Updated
    Accepted arguments

--- Assignments/Task2/cat_shelter.py
+++ Assignments/Task2/cat_shelter.py
@@ -1,1 +1,17 @@
+import sys
+import os
+
+if len(sys.argv) < 2:
+    print("Missing command line argument")
+    quit()
+
+filename = sys.argv[1]
+acceptedFiles = ['cat_shelter_1.txt', 'cat_shelter_2.txt', 'cat_shelter_3.txt']
+
+if filename not in acceptedFiles:
+    print("Cannot open that file")
+    quit()
+else:
+    print("Log File:", filename)
+
 print("Log File Analysis")
