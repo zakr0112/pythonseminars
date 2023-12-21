de692a959f9b44082ce4e1afb4cd3f773bc7664d
zakr0112 <zakofzaaz@gmail.com>
2023-12-21 17:25:46 +0000

    Updated

--- Assignments/Task2/cat_shelter.py
+++ Assignments/Task2/cat_shelter.py
@@ -4,27 +4,36 @@ if len(sys.argv) < 2:
     print("Missing command line argument")
     quit()
 
+
 filename = sys.argv[1]
 acceptedFiles = ['cat_shelter_1.txt', 'cat_shelter_2.txt', 'cat_shelter_3.txt']
-"""
 if filename not in acceptedFiles:
     print("Cannot open that file")
     quit()
 
-"""
+
 #Defining Variables for output
 myCatVisits = 0
 otherCatVisits = 0
+totalVisits = 0
 totalTime = 0
 averageVisit = 0
 longestVisit = 0
 shortestVisit = 0
 
-
+"""
 print("Log File:", filename);print()  
 fileObject = open(filename, 'r')
 fileRead = fileObject.readline()
 print(fileRead)
+"""
 
-print("Log File Analysis")
+with open(filename) as file:
+  for item in file:
+    if item != "END":
+      totalVisits = totalVisits + 1
+    print(item)
 
+print("Total Visits:", totalVisits)
+
+#print("Log File Analysis")
